2323a22987720e5430187ab7c48d0e8b0f12ac87
HyoungukJo <whguddnr9@gmail.com>
2024-12-23 21:31:09 +0900

    feat: first commit

---
+++ src/main/java/com/carvana/carvana/CarvanaApplication.java
@@ -0,0 +1,13 @@
+package com.carvana.carvana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarvanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarvanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carvana/carvana/CarvanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carvana.carvana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarvanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
