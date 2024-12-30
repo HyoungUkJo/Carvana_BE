11ea7a6f5449cde00b31806efcce240fce526cdf
HyoungukJo <whguddnr9@gmail.com>
2024-12-30 17:39:34 +0900

    feat: 회원가입 DTO를 작성한다.

---
+++ src/main/java/com/carvana/domain/user/auth/dto/SignUpRequestDto.java
@@ -0,0 +1,23 @@
+package com.carvana.domain.user.auth.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SignUpRequestDto {
+    private String email;
+    private String password;
+    private String name;
+    private String phone;
+
+    @Builder
+    public SignUpRequestDto(String email, String password,
+                            String name, String phone) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+    }
+}
