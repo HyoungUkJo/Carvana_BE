1a1367fadd5d0a78e8832950d28c12a28b85cc6e
HyoungukJo <whguddnr9@gmail.com>
2025-01-07 18:06:19 +0900

    feat: 세차장 리뷰에 대한 비즈니스로직을 작성한다.

---
+++ src/main/java/com/carvana/domain/review/service/ReviewService.java
@@ -0,0 +1,33 @@
+package com.carvana.domain.review.service;
+
+import com.carvana.domain.review.dto.ReviewResponseDto;
+import com.carvana.domain.review.entity.Review;
+import com.carvana.domain.review.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    //세차장 리뷰 전체 읽어오는 서비스 -> 추후 페이지네이션으로
+    public List<ReviewResponseDto> reviews(Long carWashId) {
+        List<Review> reviews = reviewRepository.findByCarWashIdOrderByCreatedAtDesc(carWashId);
+
+        return reviews.stream()
+            .map(review -> ReviewResponseDto.builder()
+            .customerName(review.getCustomerMember().getName())
+            .rating(review.getRating())
+            .content(review.getContent())
+            .createdAt(review.getCreatedAt())
+            .imageUrls(review.getImageUrls())
+            .build())
+            .collect(Collectors.toList());
+    }
+
+}
