4e02cabc570178420b4f7400c8a9a0d63a5230fa
HyoungukJo <whguddnr9@gmail.com>
2025-01-08 18:24:17 +0900

    feat: 세차장의 리뷰를 요청하는 api를 만든다.

--- src/main/java/com/carvana/domain/review/controller/ReviewController.java
+++ src/main/java/com/carvana/domain/review/controller/ReviewController.java
@@ -1,7 +1,26 @@
 package com.carvana.domain.review.controller;
 
+import com.carvana.domain.review.dto.ReviewResponseDto;
+import com.carvana.domain.review.repository.ReviewRepository;
+import com.carvana.domain.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/review")
 public class ReviewController {
+    private final ReviewRepository reviewRepository;
+    private final ReviewService reviewService;
+
+    // 전체 리뷰 return
+    @GetMapping("/getAllReviews/{carWashId}")
+    public List<ReviewResponseDto> getAllReviews(@PathVariable Long carWashId){
+        return reviewService.getAllReviews(carWashId);
+    }
 }
