ee24ec66cae3d3e79f96da5ef563f396d8383b4f
HyoungukJo <whguddnr9@gmail.com>
2025-01-08 18:20:11 +0900

    feat: 특정 기간의 예약 쿼리를 추가한다.

--- src/main/java/com/carvana/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/carvana/domain/reservation/repository/ReservationRepository.java
@@ -4,6 +4,7 @@ import com.carvana.domain.reservation.entity.Reservation;
 import com.carvana.domain.reservation.entity.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
@@ -11,4 +12,12 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     // 특정 예약 상태의 예약을 조회
     List<Reservation> findByCarWashIdAndStatusOrderByCreateAtDesc(Long carWashId, ReservationStatus status);
+
+    // 특정 기간과 특정 예약상태의 예약을 조회
+    List<Reservation> findByCarWashIdAndStatusAndReservationDateTimeBetweenOrderByReservationDateTime(
+        Long carWashId,
+        ReservationStatus status,
+        LocalDateTime startOfMonth,
+        LocalDateTime endOfMonth
+    );
 }
