360984209549e9cb523e5e4d7a6dc5e85d7931f2
HyoungukJo <whguddnr9@gmail.com>
2025-01-08 18:24:52 +0900

    feat: 특정 기간의 리뷰를 가지고오는 쿼리를 작성한다.

--- src/main/java/com/carvana/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/carvana/domain/review/repository/ReviewRepository.java
@@ -3,12 +3,14 @@ package com.carvana.domain.review.repository;
 import com.carvana.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     // 리뷰 일자 정렬
+    // 추후 페이지네이션 적용 (프론트와 합의 필요)
     List<Review> findByCarWashIdOrderByCreatedAtDesc(Long carWashId);
 
-    // 추후 페이지네이션 적용 (프론트와 합의 필요)
+    int countByCarWashIdAndCreatedAtBetween(Long carWashId, LocalDateTime startOfMonth, LocalDateTime endOfMonth);
 }
