b38534a9cded352fd30abacdaf83bfa9b8efd533
HyoungukJo <whguddnr9@gmail.com>
2025-01-09 18:37:07 +0900

    feat: 세차장 정보 기입시 인증할 ownerId와 대표이미지를 받는다.

--- src/main/java/com/carvana/domain/store/carwash/dto/RegisterCarWashRequestDto.java
+++ src/main/java/com/carvana/domain/store/carwash/dto/RegisterCarWashRequestDto.java
@@ -6,8 +6,10 @@ import lombok.Getter;
 @Getter
 @Builder
 public class RegisterCarWashRequestDto {
+    private Long ownerId; // 임시로 ownerId를 요청
     private String name;
     private String address;
     private String phone;
     private String businessHours;
+    private String thumbnailImgUrl; // 대표 이미지(썸네일)
 }
