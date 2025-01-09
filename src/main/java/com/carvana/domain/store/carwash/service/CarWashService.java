e59240acfbfcb33d36213b78cf5ecf45412ff4d0
HyoungukJo <whguddnr9@gmail.com>
2025-01-09 18:35:16 +0900

    feat: 프로필 전송 및 수정 로직을 작성한다.

--- src/main/java/com/carvana/domain/store/carwash/service/CarWashService.java
+++ src/main/java/com/carvana/domain/store/carwash/service/CarWashService.java
@@ -2,15 +2,13 @@ package com.carvana.domain.store.carwash.service;
 
 import com.carvana.domain.owner.member.entity.OwnerMember;
 import com.carvana.domain.owner.member.repository.OwnerMemberRepository;
-import com.carvana.domain.store.carwash.dto.RegisterCarWashMenuRequestDto;
-import com.carvana.domain.store.carwash.dto.RegisterCarWashMenuResponseDto;
-import com.carvana.domain.store.carwash.dto.RegisterCarWashRequestDto;
-import com.carvana.domain.store.carwash.dto.RegisterCarWashResponseDto;
+import com.carvana.domain.store.carwash.dto.*;
 import com.carvana.domain.store.carwash.entity.CarWash;
 import com.carvana.domain.store.carwash.entity.CarWashMenu;
 import com.carvana.domain.store.carwash.repository.CarWashMenuRepository;
 import com.carvana.domain.store.carwash.repository.CarWashRepository;
 import com.carvana.global.exception.custom.IncorrectEmailPasswordException;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,15 +20,16 @@ public class CarWashService {
     private final CarWashRepository carWashRepository;
     private final OwnerMemberRepository ownerMemberRepository;
     private final CarWashMenuRepository carWashMenuRepository;
-    public RegisterCarWashResponseDto registerCarWash(RegisterCarWashRequestDto registerCarWashRequestDto, Long ownerId) {
+    public RegisterCarWashResponseDto registerCarWash(RegisterCarWashRequestDto registerCarWashRequestDto) {
         // 실제 오너가 있는지 검증
-        OwnerMember ownerMember = ownerMemberRepository.findById(ownerId).orElseThrow(() -> new IncorrectEmailPasswordException("아이디 또는 비밀번호가 틀립니다."));
+        OwnerMember ownerMember = ownerMemberRepository.findById(registerCarWashRequestDto.getOwnerId()).orElseThrow(() -> new IncorrectEmailPasswordException("아이디 또는 비밀번호가 틀립니다."));
         // 추후 중복되는 주소 또는 번호로 검증해야할듯?
         CarWash carWash = CarWash.builder()
             .name(registerCarWashRequestDto.getName())
             .phone(registerCarWashRequestDto.getPhone())
             .address(registerCarWashRequestDto.getAddress())
             .businessHours(registerCarWashRequestDto.getBusinessHours())
+            .thumbnailImgUrl(registerCarWashRequestDto.getThumbnailImgUrl())
             .build();
 
         ownerMember.addCarWash(carWash);
@@ -57,4 +56,36 @@ public class CarWashService {
 
         return RegisterCarWashMenuResponseDto.builder().carWashId(carWash.getId()).carWashMenuId(carWashMenu.getId()).build();
     }
+
+    // 세차장 프로필
+    public CarWashProfileResponseDto getCarWashProfile(Long carWasId){
+        CarWash carWash = carWashRepository.findById(carWasId).orElseThrow(() -> new EntityNotFoundException("해당하는 세차장이 없습니다."));
+        OwnerMember ownerMember = carWash.getOwnerMember();
+
+        return CarWashProfileResponseDto.builder()
+            .name(carWash.getName())
+            .businessNumber(ownerMember.getBusinessNumber())
+            .address(carWash.getAddress())
+            .bayCount(carWash.getBayCount())
+            .phone(carWash.getPhone())
+            .businessHours(carWash.getBusinessHours())
+            .build();
+    }
+
+
+    // 세차장 프로필 수정
+    public CarWashProfileResponseDto updateCarWashProfile(Long carWashId,CarWashProfileUpdateRequestDto dto) {
+        CarWash carWash = carWashRepository.findById(carWashId).orElseThrow(() -> new EntityNotFoundException("해당하는 세차장이 없습니다."));
+        carWash.updateCarWash(dto);
+
+        return CarWashProfileResponseDto.builder()
+            .name(carWash.getName())
+            .businessNumber(carWash.getOwnerMember().getBusinessNumber())
+            .address(carWash.getAddress())
+            .bayCount(carWash.getBayCount())
+            .phone(carWash.getPhone())
+            .businessHours(carWash.getBusinessHours())
+            .build();
+    }
+
 }
