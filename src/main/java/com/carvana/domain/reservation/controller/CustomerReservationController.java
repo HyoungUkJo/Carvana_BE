1a7816137d6caa4991c10bfaed1fe2cd9635f8ba
HyoungukJo <whguddnr9@gmail.com>
2025-01-12 20:02:18 +0900

    feat: 내가 예약한 예약 API를 작성한다.

--- src/main/java/com/carvana/domain/reservation/controller/CustomerReservationController.java
+++ src/main/java/com/carvana/domain/reservation/controller/CustomerReservationController.java
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Tag(name = "손님 예약", description = "예약과 관련된 API")
 @RestController
 @RequiredArgsConstructor
@@ -25,4 +27,11 @@ public class CustomerReservationController {
         * */
         return reservationService.createReservation(requestDto);
     }
+
+    // 내 예약 확인
+    @GetMapping("/myReservation/{customerId}")
+    public List<ReservationResponseDto> getMyReservation(@PathVariable Long customerId) {
+        return reservationService.getMyReservation(customerId);
+    }
+
 }
