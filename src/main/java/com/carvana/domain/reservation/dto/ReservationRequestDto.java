d0fb46e0912e7f8e1f32254b1ea0be447d6696d0
HyoungukJo <whguddnr9@gmail.com>
2025-01-19 12:17:27 +0900

    refactor: 예약 메뉴를 여러개 선택할 수 있도록 변경한다. 차 번호를 입력 받는다.

--- src/main/java/com/carvana/domain/reservation/dto/ReservationRequestDto.java
+++ src/main/java/com/carvana/domain/reservation/dto/ReservationRequestDto.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -14,7 +15,8 @@ public class ReservationRequestDto {
     private Long customerMemberID;      // 예약자 정보 -> Todo: 추후 토큰으로 대체
     private Long carWashId;             // 세차장 아이디
     private String carType;             // 차종
-    private Long menuId;                // 메뉴 아이디
+    private String carNumber;           // 차번호
+    private List<Long> menuIds;         // 선택한 메뉴 아이디
     private LocalDateTime reservationDateTime;  // 예약일자
     private String request;             //  요청사항
     private Integer bayNumber;          // 예약할 베이 번호
