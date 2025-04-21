17edcbb50092418f2e57fa184f5c8dc581b7a2b7
HyoungUkJo <whguddnr9@gmail.com>
2025-04-21 22:58:47 +0900

    feat: Carwash에 대한 간략한 정보를 response하는 dto를 작성한다.

--- src/main/java/com/carvana/domain/store/carwash/dto/CarWashResponseDto.java
+++ src/main/java/com/carvana/domain/store/carwash/dto/CarWashResponseDto.java
@@ -1,8 +1,33 @@
 package com.carvana.domain.store.carwash.dto;
 
+import com.carvana.domain.store.carwash.entity.CarWash;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CarWashResponseDto {
+    private Long id;
+    private String name;
+    private String address;       // full address
+    private String region;
+    private String district;
+    private String thumbnailImgKey;
+    private Double rating;
+    private Integer minPrice;
+    private Integer maxPrice;
 
+    public static CarWashResponseDto from(CarWash carWash) {
+        return CarWashResponseDto.builder()
+            .id(carWash.getId())
+            .name(carWash.getName())
+            .address(carWash.getAddress())
+            .region(carWash.getRegion())
+            .district(carWash.getDistrict())
+            .thumbnailImgKey(carWash.getThumbnailImgKey())
+            .rating(carWash.getRating())
+            .minPrice(carWash.getMinPrice())
+            .maxPrice(carWash.getMaxPrice())
+            .build();
+    }
 }
